64b558b139ddc5929af5bd8ae40138c334e4075f
Kuan Hua Wang <56338617+wangkuanhua@users.noreply.github.com>
2023-08-17 14:34:19 -0500

    Update DV0101EN-Final_Assign_Part_2_Questions.py

--- 08. Data Visualization with Python/Week 5 - Final Project ＆ Exam/DV0101EN-Final_Assign_Part_2_Questions.py
+++ 08. Data Visualization with Python/Week 5 - Final Project ＆ Exam/DV0101EN-Final_Assign_Part_2_Questions.py
@@ -33,16 +33,15 @@ year_list = [i for i in range(1980, 2024, 1)]
 # Create the layout of the app
 app.layout = html.Div([
     #TASK 2.1 Add title to the dashboard
-    html.H1("Automobile Statistics Dashboard", 
-            style={'textAlign': 'center', 'color': '#503D36',
-                                'font-size': 26}),
+    html.H1("Automobile Sales Statistics Dashboard", style={'textAlign': 'center','color':'#503D36', 'font-size':'24'}),#May include style for title
     html.Div([#TASK 2.2: Add two dropdown menus
         html.Label("Select Statistics:"),
         dcc.Dropdown(
             id='dropdown-statistics',
             options=dropdown_options,
             value='Select Statistics',
-            )
+            placeholder='Select a report type'
+        )
     ]),
     html.Div(dcc.Dropdown(
             id='select-year',
@@ -50,8 +49,9 @@ app.layout = html.Div([
             value='select-year'
         )),
     html.Div([#TASK 2.3: Add a division for output display
-    html.Div(id='output-container', className='chart-grid', style={'display': 'flex'}),])
+    html.Div(id='output-container', className='chart-grid', style={'display':'flex'}),])
 ])
+
 #TASK 2.4: Creating Callbacks
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
@@ -69,62 +69,86 @@ def update_input_container(selected_statistics):
 @app.callback(
     Output(component_id='output-container', component_property='children'),
     [Input(component_id='dropdown-statistics', component_property='value'), 
-     Input(component_id='select-year', component_property='value')])
-
+    Input(component_id='select-year', component_property='value')])
 
 def update_output_container(selected_statistics, input_year):
     if selected_statistics == 'Recession Period Statistics':
         # Filter the data for recession periods
         recession_data = data[data['Recession'] == 1]
-        
+
+
 #TASK 2.5: Creating Graphs for Recession data
 
-#Plot 1 Automobile sales fluctuate over Recession Period (year wise)
-        # use groupby to create relevant data for plotting
+#Plot 1 Automobile sales fluctuate over Recession Period (year wise) using line chart
+         # grouping data for plotting
         yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        # Plotting the line graph
         R_chart1 = dcc.Graph(
             figure=px.line(yearly_rec, 
-                x='Year',
-                y='Automobile_Sales',
-                title="Average Automobile Sales fluctuation over Recession Period"))
+                x='Year', y='Automobile_Sales',
+                title='Average Automobile Sales fluctuation over Recession Period'))
 
-#Plot 2 Calculate the average number of vehicles sold by vehicle type       
-        # use groupby to create relevant data for plotting
+#Plot 2 Calculate the average number of vehicles sold by vehicle type and represent as a Bar chart                                      
         average_sales = recession_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()                           
-        R_chart2  = dcc.Graph(figure=px.bar(average_sales,
-                x='Vehicle_Type',
-                y='Automobile_Sales',
-                title='Average Number of Vehicles Sold by Type of Vehicle'))
-        
-# Plot 3 Pie chart for total expenditure share by vehicle type during recessions
-        # use groupby to create relevant data for plotting
-        exp_rec = recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
+        R_chart2  = dcc.Graph(
+                figure=px.bar(average_sales, x='Vehicle_Type', y='Automobile_Sales',
+                title='Average Number of Vehicles Sold by Type'))
+
+# Plot 3 : Pie chart for total expenditure share by vehicle type during recessions
+            # grouping data for plotting
+        exp_rec= recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
         R_chart3 = dcc.Graph(
-                figure=px.pie(exp_rec,
-                values='Advertising_Expenditure',
-            names='Vehicle_Type',
-            title="Total Advertising Expense by Vehicle Type"))
+                    figure=px.pie(exp_rec, values='Advertising_Expenditure', names= 'Vehicle_Type', 
+                    title='Recessionary Expenditure Percentage by Vehicle Type')
+        )
 
-# Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
-        unemp = recession_data.groupby('Vehicle_Type','unemployment_rate')['Automobile_Sales'].Count()
+# Plot 4 Develop a Bar chart for the effect of unemployment rate on vehicle type and sales
+        unemp = recession_data.groupby(['Vehicle_Type', 'unemployment_rate'])['Automobile_Sales'].mean().reset_index()
         R_chart4 = dcc.Graph(
-            figure=px.bar(unemp,
-            x='unemployment_rate',
-            y='Automobile_Sales',
-            title='Effect of Unemployment Rate on Sales of Vehicle Types'))
+                    figure=px.bar(unemp,
+                    x='unemployment_rate',
+                    y='Automobile_Sales',
+                    title='Effect of Unemployment on Sales of Vehicle Types'))
 
-#TASK 2.6: Returning the graphs for displaying Recession data
+ #TASK 2.6: Returning the graphs for displaying Recession data
         return [
             html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)],style={'display':'flex'}),
             html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)],style={'display':'flex'})
-            ]
- # Yearly Statistic Report Plots                             
+            ]    
+        
+  # Yearly Statistic Report Plots                             
     elif (input_year and selected_statistics=='Yearly Statistics') :
         yearly_data = data[data['Year'] == input_year]
-    else:
-        return None    
                               
 
+# Plot 1 :Yearly Automobile sales using line chart for the whole period.
+        yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
+        Y_chart1 = dcc.Graph(figure=px.line(yas, x='Year', y='Automobile_Sales',
+        title='Yearly Automobile Sales'))
+            
+# Plot 2 :Total Monthly Automobile sales using line chart.
+        Y_chart2 = dcc.Graph(figure=px.line(yearly_data, x='Month',
+                    y='Automobile_Sales', title="Monthly Automobile Sales for the year {}".format(input_year)))
+
+# Plot bar chart for average number of vehicles sold during the given year
+  
+        avr_vdata=yearly_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()
+        Y_chart3 = dcc.Graph( figure=px.bar(avr_vdata, x='Vehicle_Type', y='Automobile_Sales',
+                    title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
+
+# Plot 4 Total Advertisement Expenditure for each vehicle using pie chart
+        
+        exp_data=yearly_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset.index()
+        Y_chart4 = dcc.Graph(figure=px.pie(exp_data, values='Advertising_Expenditure', names='Vehicle_Type',
+        title='Advertising Expense per Vehicle Type'))
+
+#TASK 2.6: Returning the graphs for displaying Recession data
+        return [
+            html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display':'flex'}),
+            html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display':'flex'})
+            ]    
+    else:
+        return None
 
 # Run the Dash app
 if __name__ == '__main__':
